a27a8eb27d4899a5a0c3fc62cce401dbdfac867d
karinamatsiash <98473853+karinamatsiash@users.noreply.github.com>
2022-02-02 16:03:23 +0300

    Init project (#4)
    * Add angular materials
    
    * Fix unit tests and add test coverage
    
    * Update husky
    
    * Add tests and create pre-push hook

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,32 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgxsModule } from '@ngxs/store';
+import { TitleState } from '@core/store/title';
+import { of } from 'rxjs';
 
 describe('AppComponent', () => {
+	let fixture: ComponentFixture<AppComponent>;
+	let component: AppComponent;
+
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
 			declarations: [AppComponent],
+			imports: [TranslateModule.forRoot(), NgxsModule.forRoot([TitleState])],
 		}).compileComponents();
+
+		fixture = TestBed.createComponent(AppComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
 	});
 
 	it('should create the app', () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		const app = fixture.componentInstance;
-		expect(app).toBeTruthy();
+		expect(component).toBeTruthy();
 	});
 
 	it(`should have as title 'travel'`, () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		const app = fixture.componentInstance;
-		expect(app.titleName).toEqual('TRAVEL');
-	});
-
-	it('should render title', () => {
-		const fixture = TestBed.createComponent(AppComponent);
+		spyOnProperty(component, 'titleName$').and.returnValue(of('TRAVEL'));
 		fixture.detectChanges();
-		const compiled = fixture.nativeElement as HTMLElement;
-		expect(compiled.querySelector('.content span')?.textContent).toContain('travel app is running!');
+		expect(component.titleName).toEqual('TRAVEL');
 	});
 });
